723058b6a3c62516764e63e7001fca7694120235
AlexDNZ <paleozene@gmail.com>
2019-06-14 22:08:10 +0200

    Config for sidekiq deployment Create and set config/initializers/redis.rb Add Sidekiq.config for server and client in config/sidekiq.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # require 'sidekisq/web'
+  # mount Sidekiq::Web => '/sidekiq'
+
   root to: 'home#index'
   resources :home, only: %i[index create]
 end
