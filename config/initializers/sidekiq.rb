c229738c30e052a329b5db713f9b700945814678
AlexDNZ <paleozene@gmail.com>
2019-06-15 01:02:13 +0200

    change sidekiq.rb config arrangement

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,14 +2,13 @@
 
 Sidekiq.configure_server do |config|
   config.redis = { url: ENV['REDIS_PROVIDER'] }
+
+  schedule_file = "config/schedule.yml"
+  if File.exist?(schedule_file) && Sidekiq.server?
+    Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
+  end
 end
 
 Sidekiq.configure_client do |config|
   config.redis = { url: ENV['REDIS_PROVIDER'] }
-end
-
-schedule_file = "config/schedule.yml"
-
-if File.exist?(schedule_file) && Sidekiq.server?
-  Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
 end
