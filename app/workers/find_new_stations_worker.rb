d335ec598b2cb2bef9e602beb32cbdc46f0f787c
AlexDNZ <paleozene@gmail.com>
2019-06-18 01:05:59 +0200

    Add new kind of button in view Set responsive div for main_map in home.scss Update workers for colours in term Config schedule for looking for new stations each 12h

--- app/workers/find_new_stations_worker.rb
+++ app/workers/find_new_stations_worker.rb
@@ -11,15 +11,15 @@ class FindNewStationsWorker
     stations = velibs["stations"]
 
     number_new_stations = 0
-    puts "-------------------------------------------------"
-    puts "RESEARCH FOR NEW STATIONS ... ".green.blink
-    puts "BEGINS AT : #{Time.zone.now}".green.blink
+    puts "*********************************************".cyan
+    puts "RESEARCH FOR NEW STATIONS ... ".cyan
+    puts "BEGINS AT : #{Time.zone.now}".cyan
     stations.each do |e|
       if !Station.exists?(identification: e['id'])
         Station.create(identification: e['id'], name: e["name"], latitude: e["latitude"], longitude: e["longitude"], vacant_bikes: e["free_bikes"])
       end
     end
-    puts "NOMBRE DE NOUVELLE STATIONS AJOUTÉES : #{number_new_stations}".green.blink
-    puts "FINISHED AT :#{Time.zone.now}".green.blink
+    puts "NOMBRE DE NOUVELLE STATIONS AJOUTÉES : #{number_new_stations}".cyan
+    puts "FINISHED AT :#{Time.zone.now}".cyan
   end
 end
