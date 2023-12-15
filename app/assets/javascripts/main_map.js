baee62b12c54549fb7467a00f2a20d60c623870a
AlexDNZ <paleozene@gmail.com>
2023-12-16 00:16:34 +0100

    maintenance: fix tileLayer with valid URL

--- app/assets/javascripts/main_map.js
+++ app/assets/javascripts/main_map.js
@@ -1,24 +1,33 @@
-//console.log("hello from main_map.js");
+const thp_logo = "https://images.crunchbase.com/image/upload/c_lpad,h_170,w_170,f_auto,b_white,q_auto:eco,dpr_1/bn6lwjqsl2d8fpffdf0o"
 var map;
 var centerPoint = [gon.thp_location.latitude, gon.thp_location.longitude]
 var thpMarker ;
 var genericMarker ;
 var markers;
 
+
+const map_layer_arr = [
+  "https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png",
+  "https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png",
+  'https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png',
+  'https://{s}.tile-cyclosm.openstreetmap.fr/cyclosm/{z}/{x}/{y}.png',
+  ]
+
 function createMap(coordinates , boolean)
 {
   map = L.map('main-map').setView(coordinates, 16);
-  L.tileLayer('https://{s}.tile.openstreetmap.se/hydda/full/{z}/{x}/{y}.png',{maxZoom :100}).addTo(map);
+  layer = L.tileLayer(map_layer_arr[0],{maxZoom :100})
+  layer.addTo(map);
   markers = L.layerGroup();
 
   thpIcon = L.icon({
-      iconUrl: 'https://i2.wp.com/laboate.com/next3/wp-content/uploads/2018/05/hacking-project.png?fit=240%2C239',
+      iconUrl: thp_logo,
       iconSize: [35, 35],
       iconAnchor: [22, 38],
       popupAnchor: [-3, -76],
       shadowSize: [68, 95],
       shadowAnchor: [22, 94]
-      });
+    });
 
   if (boolean == true)
   {thpMarker = L.marker(coordinates, {icon: thpIcon})}
